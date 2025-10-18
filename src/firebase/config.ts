1d75f906d9a282a62009b54024a693f9fe10cf9c
Eberhard Janzen <eberhard.janzen50@gmail.com>
2025-10-18 03:52:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6151698579-c04b2",
+  "appId": "1:788829916267:web:de15ed2ddb06cd2078352e",
+  "apiKey": "AIzaSyDrFXnwNvXZyukgKLOvEk39rpRcAEn1JJo",
+  "authDomain": "studio-6151698579-c04b2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "788829916267"
+};
