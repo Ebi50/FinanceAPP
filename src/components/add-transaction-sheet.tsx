d324da3ef6eee036f5c369daddf3862b79d1bb92
Eberhard Janzen <eberhard.janzen50@gmail.com>
2025-10-25 09:04:57 +0000

    Wenn ich hier eine position der beträge hinzufüge, dann öffnet sich kein

--- src/components/add-transaction-sheet.tsx
+++ src/components/add-transaction-sheet.tsx
@@ -218,9 +218,9 @@ export function AddTransactionSheet({
         </SheetHeader>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="flex-1 flex flex-col"
+          className="flex-1 flex flex-col overflow-hidden"
         >
-          <div className="flex-1 overflow-y-auto pr-4 -mr-6 pl-1 -ml-1">
+          <div className="flex-1 overflow-y-auto pr-6 pl-1 -mr-6 -ml-1">
             <div className="grid gap-4 py-4">
               
               <div className="space-y-2">
